b63a1f6962497f710499187981a4d8e38aefb058
RedBoard <ott.thomas68@gmail.com>
2023-04-29 01:39:37 +0200

    (+) function to remove datafolder and add license

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,23 @@ All notable changes to the "autonormecompliance" extension will be documented in
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [1.0.0] - 2023-04-30
+## [1.0.1] - 2023-04-29
+
+### Added
+
+- function to remove the datafolder
+
+## [1.0.1] - 2023-04-29
+
+### Fixed
+
+- Install dockerode package
+
+### Added
+
+- Set line where coding style was found in comment
+
+## [1.0.0] - 2023-04-29
 
 ### Added
 
--- extension.js
+++ extension.js
@@ -6,43 +6,71 @@ const path = require('path');
 const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
 async function runDockerScript(deliveryDir, reportsDir) {
-    try {
-        const container = await docker.createContainer({
-            Image: 'ghcr.io/epitech/coding-style-checker:latest',
-            Cmd: ['/mnt/delivery', '/mnt/reports'],
-            HostConfig: {
-                Binds: [
-                    `${deliveryDir}:/mnt/delivery`,
-                    `${reportsDir}:/mnt/reports`
-                ]
-            },
-            AttachStdout: true,
-            AttachStderr: true
-        });
-        await container.start();
-        const stream = await container.attach({ stream: true, stdout: true, stderr: true });
-        container.modem.demuxStream(stream, process.stdout, process.stderr);
-        stream.on('end', async () => {
-            await container.remove();
-        });
-    } catch (error) {
-        vscode.window.showErrorMessage('Failed to run Docker script: ' + error.message);
+    return new Promise(async (resolve, reject) => {
+        try {
+            const container = await docker.createContainer({
+                Image: 'ghcr.io/epitech/coding-style-checker:latest',
+                Cmd: ['/mnt/delivery', '/mnt/reports'],
+                HostConfig: {
+                    Binds: [
+                        `${deliveryDir}:/mnt/delivery`,
+                        `${reportsDir}:/mnt/reports`
+                    ]
+                },
+                AttachStdout: true,
+                AttachStderr: true
+            });
+            await container.start();
+            const stream = await container.attach({stream: true, stdout: true, stderr: true});
+            container.modem.demuxStream(stream, process.stdout, process.stderr);
+
+            stream.on('end', async () => {
+                await container.remove();
+                resolve();
+            });
+        } catch (error) {
+            vscode.window.showErrorMessage('Failed to run Docker script: ' + error.message);
+            reject(error);
+        }
+    });
+}
+
+async function commentLines(deliveryDir, reportFilePath) {
+    const reportContent = fs.readFileSync(reportFilePath, 'utf-8');
+    const lines = reportContent.split('\n');
+
+    for (const line of lines) {
+        const match = line.match(/^(.+\.c):(\d+):/);
+        if (match) {
+            const filePath = path.join(deliveryDir, match[1]);
+            const lineNumber = parseInt(match[2], 10) - 1;
+            try {
+                const document = await vscode.workspace.openTextDocument(filePath);
+                const editor = await vscode.window.showTextDocument(document);
+                await editor.edit((editBuilder) => {
+                    const lineLength = document.lineAt(lineNumber).range.end.character;
+                    editBuilder.insert(new vscode.Position(lineNumber, lineLength), ' // CODING STYLE');
+                });
+            } catch (error) {
+                vscode.window.showErrorMessage(`Failed to comment line in file: ${filePath}, error: ${error.message}`);
+            }
+        }
     }
 }
 
-// function deleteFolderRecursive(folderPath) {
-//     if (fs.existsSync(folderPath)) {
-//         fs.readdirSync(folderPath).forEach((file) => {
-//             const curPath = path.join(folderPath, file);
-//             if (fs.lstatSync(curPath).isDirectory()) {
-//                 deleteFolderRecursive(curPath);
-//             } else {
-//                 fs.unlinkSync(curPath);
-//             }
-//         });
-//         fs.rmdirSync(folderPath);
-//     }
-// }
+function deleteFolderRecursive(folderPath) {
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(folderPath);
+    }
+}
 
 function activate(context) {
     let disposable = vscode.commands.registerCommand('extension.checkCodingStyle', async () => {
@@ -59,10 +87,9 @@ function activate(context) {
         await runDockerScript(deliveryDir, reportsDir);
         const reportFilePath = path.join(reportsDir, 'coding-style-reports.log');
         if (fs.existsSync(reportFilePath)) {
-            const reportContent = fs.readFileSync(reportFilePath, 'utf-8');
             vscode.window.showInformationMessage(`Coding style report generated at: ${reportFilePath}`);
-            const document = await vscode.workspace.openTextDocument(reportFilePath);
-            vscode.window.showTextDocument(document);
+            await commentLines(deliveryDir, reportFilePath);
+            deleteFolderRecursive(reportsDir);
         } else {
             vscode.window.showErrorMessage('Report file not found.');
         }
